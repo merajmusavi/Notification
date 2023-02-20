530ff69d3ed8f40157ad6e62b07765ca22c0c3fb
merajuni7ed <merajmusavi1243m@gmail.com>
2023-02-20 15:34:57 +0330

    declare an action for notification

--- app/src/main/java/com/example/notification/MainActivity.java
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.core.app.NotificationCompat;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
@@ -21,12 +23,17 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),0,intent,0);
+
                 Notification notification = new NotificationCompat.Builder(getApplicationContext(),"myapp")
                         .setSmallIcon(R.drawable.ic_launcher_background)
                         .setLargeIcon(BitmapFactory
                                 .decodeResource(getResources(),R.mipmap.ic_launcher))
                         .setContentTitle("fsasfasfd")
                         .setContentText("sadfsefewfwsfewswfc")
+                        .setContentIntent(pendingIntent)
                         .build();
 
                 NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
